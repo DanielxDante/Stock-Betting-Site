f9b70f02f9666b5000c031d887b9184fa01d09ba
DanielxDante <danielonline170600@outlook.com>
2022-01-08 18:25:06 +0800

    8/1/2021

--- website/views.py
+++ website/views.py
@@ -1,7 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user #current_user can only be used if we use UserMixin in moddels.py
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__) #dont have to name it like this, __name__ is syntax
 
-@views.route('/') # / stands for home page
+@views.route('/', methods=['GET', 'POST']) # / stands for home page
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method =='POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category = 'error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId'] #json creates a dictionary that includes our noteID and its data
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({}) #empty response to show success or error; flask requires to return something
